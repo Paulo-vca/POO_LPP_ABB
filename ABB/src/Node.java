d8df780855a98bc0d7e44f83883c093e0d4f2134
Paulo_vca <paulovca26@gmail.com>
2022-12-05 21:44:51 -0400

    05/12

---
+++ ABB/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int info;
+    Node esq;
+    Node dir;
+
+    public Node(int info){
+        this.info = info;
+        this.dir = null;
+        this.esq = null;
+    }
+}
