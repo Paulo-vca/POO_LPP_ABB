cb4a7e04a48b285282e81246ca94d7c486e3f473
Paulo_vca <paulovca26@gmail.com>
2022-12-07 19:40:56 -0400

    07/12

--- ABB/src/criarTAbela.sql
+++ ABB/src/criarTAbela.sql
@@ -1,27 +1,10 @@
-CREATE TABLE
-    tb_user(
+    CREATE TABLE
+    node(
         id int not null auto_increment,
-		username varchar(255),
-        password varchar(255),
-        status int,
-        token double,
+        info int,
         primary key(id));
 
-    CREATE TABLE
-    tb_Produto(
-        id int not null auto_increment,
-		nomeProduto varchar(255),
-        marcaProduto varchar(255),
-        precoProduto float,
-        categoria varchar(255),
-        primary key(id));
 
-    CREATE TABLE
-    tb_carro(
-        id int not null auto_increment,
-		name varchar(255),
-        dt_fabricacao DATE,
-        valor double,
-        primary key(id));
+SELECT * from node;
     
     
